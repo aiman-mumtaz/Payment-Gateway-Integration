7d5bc3b5a1c930f9ca39c97e57cb97db6fc5c1c9
Aiman Mumtaz <aimanmumtaz786@gmail.com>
2020-12-09 00:39:04 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const Razorpay = require('razorpay')
+const path = require('path')
+
+let instace = new Razorpay ({
+    key_id : 'rzp_test_W0q2jqxf1ZzWFv',
+    key_secret : 'iGHMomKVuLzdYUKviAY4SoaY'
+})
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '/views'))
+
+app.use(express.json())
+app.use(express.static(__dirname + "/public"))
+app.use(express.urlencoded({extended: true}))
+
+app.get('/',(req,res) => {
+    res.render('home')
+})
+
+app.get('/donate',(req,res) => {
+    res.render('donate')
+})
+
+app.post('/donate', (req,res) => {
+
+    var options = {
+        amount : req.body.amount,
+        currency : 'INR'
+    }
+    
+    console.log(req.body)
+    instace.orders.create(options).then((data) => {
+        let details = {
+            name : req.body.name,
+            email : req.body.email,
+            amount: req.body.amount*1000,
+            mobile: req.body.mobile,
+            payerId: data.id
+        }
+        res.render('checkout', {details : details})
+        // res.send({"sub": data, "status": "success"})
+    }).catch((error) => {
+        res.send({"sub": error, "status": "failed"})
+    })
+})
+
+app.get('/success', (req,res) => {
+    res.render('success')
+})
+
+app.get('/fail', (req,res) => {
+    res.render('fail')
+})
+
+app.listen(5524,() => {
+    console.log('Server Running at http://localhost:5524')
+})
